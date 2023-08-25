42d9521b283ddc4a1a78ed86a383aabb142e007a
Vikalpwebtech <vikalprautela@gmail.com>
2023-08-25 20:16:52 +0530

    Submit code

--- src/main/java/com/driver/OrderController.java
+++ src/main/java/com/driver/OrderController.java
@@ -36,6 +36,7 @@ public class OrderController {
     public ResponseEntity<String> addOrderPartnerPair(@RequestParam String orderId, @RequestParam String partnerId){
 
         //This is basically assigning that order to that partnerId
+        orderService.addorderstopartner(orderId,partnerId);
         return new ResponseEntity<>("New order-partner pair added successfully", HttpStatus.CREATED);
     }
 
